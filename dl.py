82d6de81bbd307497dab8bc41effcc12613d6296
dugduy <102119666+dugduy@users.noreply.github.com>
2022-06-21 02:29:23 +0700

    Add files via upload
    v1 build 11

--- dl.py
+++ dl.py
@@ -1,8 +1,11 @@
 from subprocess import run
 run('@echo off&title Download Widevine video pops.vn',shell=1)
 mpd_url=input('Mpd url: ')
-run('yt-dlp %s --allow-u -o a'%mpd_url)
-cmds="mp4decrypt --key 509ff87e5c905222895cefb94f4a7afd:be7e251f461f44a5e1602ce9c2966b17 a.ff2-v1.mp4 video.mp4 & mp4decrypt --key 509ff87e5c905222895cefb94f4a7afd:be7e251f461f44a5e1602ce9c2966b17 a.ff1-a1.m4a audio.m4a & ffmpeg -i video.mp4 -i audio.m4a -c copy final.mp4"
-for i in cmds.split(' & '):
-    run(i)
-input('Done!\nYour video is output.mp4!!!')
+print('Downloading...')
+run('yt-dlp -f f1-a1,f2-v1 %s --allow-u -o a'%mpd_url)
+print('Decrypting...')
+run('mp4decrypt --key 509ff87e5c905222895cefb94f4a7afd:be7e251f461f44a5e1602ce9c2966b17 a.mp4 video.mp4')
+run('mp4decrypt --key 509ff87e5c905222895cefb94f4a7afd:be7e251f461f44a5e1602ce9c2966b17 a.m4a audio.m4a')
+print('Merging vedio and audio...')
+run("ffmpeg -i video.mp4 -i audio.m4a -c copy final.mp4")
+input('Done!\nYour video is final.mp4!!!')
